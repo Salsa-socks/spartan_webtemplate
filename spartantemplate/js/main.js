bc86bb278ce6549bb6897f89b56d727109740d9f
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-10-01 12:35:06 +0200

    got bxslider working - moved script ref to header + min.js to header

--- spartantemplate/js/main.js
+++ spartantemplate/js/main.js
@@ -8,16 +8,25 @@ $(document).ready(function()
         $(".full-nav").removeClass("open");
     });
 
-    $(window).scroll(function()
-    {
+    $(window).scroll(function(){
         var sc = $(window).scrollTop();
-        if (sc > 100)
-        {
+        if (sc > 100){
             $(".nav").addClass("sticky");
         }
-        else 
-        {
+        else{
             $(".nav").removeClass("sticky");
         }
+
+        $('.bxslider').bxSlider({
+            mode: 'horizontal',
+            moveSlides: 1,
+            slideMargin: 40,
+            infiniteLoop: true,
+            minSlides: 1,
+            maxSlides: 1,
+            speed: 1200,    
+        
+    });
+
     });
 });
